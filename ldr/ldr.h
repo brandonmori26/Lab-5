8b8a7fb494989e220259c01986e670f3eeae910c
brandonmori26 <132004285+brandonmori26@users.noreply.github.com>
2025-03-03 17:10:21 -0500

    Create ldr.h

---
+++ ldr/ldr.h
@@ -0,0 +1,12 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _carAtEntrance_H_
+#define _carAtEntrance_H_
+
+
+
+bool carAtEntrance();
+
+
+//=====[#include guards - end]=================================================
+#endif // _LIGHT_SYSTEM_H_
